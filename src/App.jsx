1d0eb3c45b2e33b20abc711c257c8c113a48a9c6
Franco Pereyra <francopereyra325@gmail.com>
2024-09-18 22:43:28 -0300

    ejercicio terminado con logica y maquetado

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,41 @@
-function App() {
-  const [count, setCount] = useState(0)
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import Titulo from './components/Titulo';
+import Formulario from './components/Formulario';
+import ListaNoticias from './components/ListaNoticias';
+import './App.css';
+
+const API_KEY = 'pub_53811818a907cd3757c64cf3d6c1821f03745';  
+const API_URL = 'https://newsdata.io/api/1/news';
+
+const App = () => {
+  const [category, setCategory] = useState('');
+  const [noticias, setNoticias] = useState([]);
+
+  useEffect(() => {
+    if (category) {
+      fetch(`${API_URL}?category=${category}&apikey=${API_KEY}`)
+        .then(response => response.json())
+        .then(data => {
+          setNoticias(data.results);
+        });
+    }
+  }, [category]);
+
+  const handleCategorySelect = (selectedCategory) => {
+    setCategory(selectedCategory);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="app">
+      <Titulo />
+      <Formulario onSelectCategory={handleCategorySelect} />
+      <ListaNoticias noticias={noticias} />
+    </div>
+  );
+};
+
+// Renderizar la aplicación directamente en el index.html
+ReactDOM.render(<App />, document.getElementById('root'));
 
 export default App
